0478faafe46cbe9035d2c638fae0155b933a17c3
Cinthia Batista <cinthia.cecilia48@gmail.com>
2025-05-01 18:51:33 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,6 @@
+let btnMenu = document.getElementById('btn-menu')
+let Menu = document.getElementById('menu-mobile')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
